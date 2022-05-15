c4d8bf17af309cef4a6b0aa2fbce3a69c3007a09
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-15 23:48:30 +0300

    add task 01.1

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+// ------------ №1 ------------
+
+const titleEl = document.querySelector('#categories');
+const listEl = titleEl.children;
+const number = listEl.length;
+
+console.log(`Кількість елементів li.item: ${number}`);
+
+// ------------ №2 ------------
+
+
+
