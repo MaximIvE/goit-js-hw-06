445099d37ed24e9935cdb71429bb35d1e5495d60
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-16 18:01:49 +0300

    add task 01.2

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,23 @@
 // ------------ №1 ------------
+//--- Спосіб 1
 
-const titleEl = document.querySelector('#categories');
-const listEl = titleEl.children;
-const number = listEl.length;
+// const titleEl = document.querySelector('#categories');
+// const listEl = titleEl.children;
+// const number = listEl.length;
+// console.log(`Number of categories: ${number}`);
 
-console.log(`Кількість елементів li.item: ${number}`);
+//--- Спосіб 2
+
+const titleEl = document.querySelectorAll('li.item');
+console.log(titleEl.length);
+console.log("");
 
 // ------------ №2 ------------
 
-
-
+for (const element of titleEl) {
+    const categoryEl = element.firstElementChild.textContent;
+    const elementsEl = element.lastElementChild.children.length;
+    console.log(`Category: ${categoryEl}`);
+    console.log(`Elements: ${elementsEl}`);
+    console.log("");
+}
