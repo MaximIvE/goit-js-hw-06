ecdd95b6a6012f58502a7a2bc86b5aa70638a9c9
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-16 23:28:29 +0300

    returns the border color to its original state in task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,20 @@
 const passwordInput = document.querySelector("#validation-input");
 
 function borderColor(){
+    // мене завжди не подобалось в інпутах, коли поичнаєш вводити дані, 
+    //а потім повністю їх витираєш, то інпут надалі підствічується червоним, тому 
+    // наступні чотири строчки ля того, щоб при повному витиранні з інпута даних, 
+    // він повертав колір бордера в початковий стан.
+
+    if(passwordInput.value.length === 0){
+        passwordInput.classList = "";
+        return;
+    };
+
     if(passwordInput.value.length == passwordInput.dataset.length){
         passwordInput.classList = "valid";
     } else {
         passwordInput.classList = "invalid";
-    }
+    };
 }
 passwordInput.addEventListener('blur', borderColor);
