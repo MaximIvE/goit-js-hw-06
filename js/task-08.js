be55e13d38ff9bd2bcd32921927479ff28cea8aa
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-17 18:54:35 +0300

    add task 7

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+const refs = {
+    formEl:         document.querySelector(".login-form"),
+    emailInput:     document.querySelector(".login-form")[0],
+    passwordInput:  document.querySelector(".login-form")[1],
+    submiButton:    document.querySelector(".login-form")[2],
+};
 
+// console.log(refs.formEl);
+// console.log(refs.emailInput);
+// console.log(refs.passwordInput);
+// console.log(refs.submiButton);
+
+function audit(){
+    // console.log("Запустилась функція валідації");
+    
+    if (refs.emailInput.value && refs.passwordInput.value){
+        return true;
+    };
+    alert("Не всі поля заповнені");
+    return false;
+};
+
+refs.formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const validation = audit();
+    if (!validation){
+        return;
+    };
+    const result = {
+        email:      refs.emailInput.value,
+        password:   refs.passwordInput.value,
+    };
+    console.log(result);
+    e.target.reset();
+  });
