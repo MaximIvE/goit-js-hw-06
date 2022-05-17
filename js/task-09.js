7f527a80741755ef94a307bc5ae80e718b6dedbc
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-05-17 19:23:48 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const buttonColor = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+// console.log(bodyEl);
+// console.log(buttonColor);
+// bodyEl.style.backgroundColor
+
+function setColor(){
+  const color = getRandomHexColor();
+  // console.log(color);
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+};
+
+buttonColor.addEventListener("click", setColor);
